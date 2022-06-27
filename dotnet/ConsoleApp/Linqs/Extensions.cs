2be55c0f6b3e5156aa674dbb1cae3a89dc764b08
ri-aashir <ridwan.ashir@outlook.com>
2022-06-27 17:36:03 +0100

    a console project to clarify "SelectMany" LINQ query

---
+++ dotnet/ConsoleApp/Linqs/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linqs
+//{
+//    internal class Extensions
+//    {
+//    }
+//}
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//namespace LinqFaroShuffle
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+
+        }
+
+        public static bool SequenceEquals<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                if (!firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ dotnet/ConsoleApp/Linqs/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Linqs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //method syntax
+            //var startingDeck = Suits().SelectMany(suit => Ranks().Select(rank => new { Suit = suit, Rank = rank }));
+
+            //query syntax
+            var startingDeck = from s in Suits()
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
+
+            // Display each card that we've generated and placed in startingDeck in the console
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine(card);
+            }
+
+
+            // 52 cards in a deck, so 52 / 2 = 26
+            var top = startingDeck.Take(26);
+            var bottom = startingDeck.Skip(26);
+
+            var shuffle = top.InterleaveSequenceWith(bottom);
+
+            //foreach (var c in shuffle)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            var times = 0;
+            // We can re-use the shuffle variable from earlier, or you can make a new one
+            shuffle = startingDeck;
+            do
+            {
+                shuffle = shuffle.Take(26).InterleaveSequenceWith(shuffle.Skip(26));
+                foreach (var card in shuffle)
+                {
+                    Console.WriteLine(card);
+                }
+                Console.WriteLine();
+                times++;
+            } while (!startingDeck.SequenceEquals(shuffle));
+            Console.WriteLine(times);
+        }
+
+    
+
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+    }
+}
