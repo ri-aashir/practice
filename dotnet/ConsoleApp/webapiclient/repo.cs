311f42c0a87ba920556309e8757a9013d9b9b5fa
ri-aashir <ridwan.ashir@outlook.com>
2022-06-27 11:36:55 +0100

    A console app that retrieves data from github API

---
+++ dotnet/ConsoleApp/webapiclient/repo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace WebAPIClient
+//{
+//    internal class repo
+//    {
+//    }
+//}
+
+namespace WebAPIClient
+{
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+
+        [JsonPropertyName("homepage")]
+        public Uri Homepage { get; set; }
+
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+
+        [JsonPropertyName("pushed_at")]
+        public DateTime LastPushUtc { get; set; }
+
+
+        public DateTime LastPush => LastPushUtc.ToLocalTime();
+
+    }
+}
