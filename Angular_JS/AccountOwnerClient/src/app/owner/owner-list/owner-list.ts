0679005ec51fd7b0ad924132ac7b39aca8bc91e5
ri-aashir <ridwaan.ashir@yahoo.com>
2025-08-18 11:48:33 +0100

    Part-10: Connecting to server side using subscription (in executing the Http request) and displaying the data

--- Angular_JS/AccountOwnerClient/src/app/owner/owner-list/owner-list.ts
+++ Angular_JS/AccountOwnerClient/src/app/owner/owner-list/owner-list.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Owner } from '../../_interfaces/owner.model';
+import { OwnerRepository } from './../../shared/services/owner-repository';
 
 @Component({
   selector: 'app-owner-list',
@@ -6,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './owner-list.html',
   styleUrl: './owner-list.css'
 })
-export class OwnerList {
+export class OwnerList implements OnInit {
+  owners: Owner[];
+  constructor(private repository: OwnerRepository) { }
+  ngOnInit(): void {
+    this.getAllOwners();
+  }
+  private getAllOwners = () => {
+    const apiAddress: string = 'api/owner';
+    this.repository.getOwners(apiAddress)
+    .subscribe(own => {
+      this.owners = own;
+    })
+  }
 
 }
