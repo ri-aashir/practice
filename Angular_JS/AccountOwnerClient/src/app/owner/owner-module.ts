1a997e0740fc140e6b331d6c51861dbb7448a624
ri-aashir <ridwaan.ashir@yahoo.com>
2025-08-18 09:12:18 +0100

    Part-10: clean up

--- Angular_JS/AccountOwnerClient/src/app/owner/owner-module.ts
+++ Angular_JS/AccountOwnerClient/src/app/owner/owner-module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OwnerRoutingModule } from './owner-routing-module';
-import { OwnerList } from './owner-list/owner-list';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    OwnerList,
-    OwnerRoutingModule
+    OwnerRoutingModule,
   ]
 })
 export class OwnerModule { }
